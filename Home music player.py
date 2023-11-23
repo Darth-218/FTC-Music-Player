a3c418b0cde1c3cb1b78ebbfdfe75ed9cef34d47
Darth218 <yahia.hany.gaber@gmail.com>
2023-11-23 11:31:06 +0200

    added border radius variable

--- Home music player.py
+++ Home music player.py
@@ -1,6 +1,5 @@
 import tkinter
 import customtkinter
-from PIL import Image
 
 
 # system settings
@@ -12,30 +11,33 @@ app = customtkinter.CTk()
 app.geometry("1440x900")
 app.title("Music player")
 
+# Variables
+border_radius = 10
+
 # Frames
 
 frame_1 = customtkinter.CTkFrame(app,fg_color='black')
 frame_1.pack(expand=True,fill='both')
 
 
-frame_2 = customtkinter.CTkFrame(frame_1,corner_radius=20)
+frame_2 = customtkinter.CTkFrame(frame_1,corner_radius=border_radius)
 frame_2.pack(side="right", fill="both", expand=True, padx=(150,420), pady=(40,95))
 
-frame_3 = customtkinter.CTkFrame(frame_1,width=120, height=200, corner_radius=20)
+frame_3 = customtkinter.CTkFrame(frame_1,width=120, height=200, corner_radius=border_radius)
 frame_3.place(relx=0.0135, rely=0.045)
 
-frame_4 = customtkinter.CTkFrame(frame_1, width=120, height=555, corner_radius=20)
+frame_4 = customtkinter.CTkFrame(frame_1, width=120, height=555, corner_radius=border_radius)
 frame_4.place(relx=0.0135, rely=0.28)
 
 frame_5 = customtkinter.CTkFrame(frame_1,width=1440, height=90, fg_color='gray')
 frame_5.place(relx=0, rely=0.9)
 
-frame_6 = customtkinter.CTkFrame(frame_1,width=360, height=760, corner_radius=20)
+frame_6 = customtkinter.CTkFrame(frame_1,width=360, height=760, corner_radius=border_radius)
 frame_6.place(relx=0.715, rely=0.045)
 
 # Link input
-link = customtkinter.CTkEntry(frame_2, width=350, height=40, font=("Systemia",15), fg_color="gray10", border_color="white", border_width=0.5, text_color="grey", corner_radius=50)
-link.place(relx=0.05, rely=0.02)     
+link = customtkinter.CTkEntry(frame_2, width=350, height=40, font=("Systemia",15), fg_color="gray10", border_color="white", border_width=0.5, text_color="grey", corner_radius=border_radius)
+link.place(relx=0.05, rely=0.02)
 
 # Dissapearing text in entry
 def temp_text(e):
