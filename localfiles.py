05cc7d69af2dbb9a8867bff34d43fcb98d315fad
Darth218 <yahia.hany.gaber@gmail.com>
2023-11-30 14:05:30 +0200

    added ability to get song path from song\queue list

--- localfiles.py
+++ localfiles.py
@@ -1,5 +1,5 @@
 """
-Testing how to select a main music folder and a song to play
+Testing the functions to select a main music folder and a song to play
 """
 
 # Imports
@@ -14,14 +14,16 @@ from tkinter import filedialog, Listbox, Tk, StringVar, Button, Label, EW
 
 root = Tk()
 root.title("FTC - Music Player")
-#root.geometry("500x400")
 
 # Functions
 
 # Funtion to get the main music folder's path
 def getfile(listbox):
 
+    global flist, pathlist
+
     flist = []
+    pathlist = []
 
     # Getting the folder path as a variable
     foldername = filedialog.askdirectory(initialdir="/", title="Select File")
@@ -29,8 +31,12 @@ def getfile(listbox):
     # At the moment of getting the directory path
     if foldername:
 
+        #Loading message
+        pathstring.set("Searching for songs...")
+        
         #Removing old files shown in the listbox
         listbox.delete(0, tk.END)
+        listbox.insert(tk.END, "Searching...")
 
         # Obtaining files to show on window from the path variable
         for path, subdir, files in os.walk(foldername):
@@ -41,12 +47,12 @@ def getfile(listbox):
                     
                     # Adds all files with ".mp3" extention to the Listbox "musiclist"
                     flist.append(os.path.join(fname))
+                    pathlist.append(os.path.join(path, fname))
+                    listbox.insert(tk.END, fname)
         print(flist, '\n')
+        print(pathlist, "\n")
 
-        # Inserts all songs in the chosen directory into the listbox
-        for song in flist:
-
-            listbox.insert(tk.END, song)
+        pathstring.set("Found songs!")
 
 
 # Function used to get the name of the selected file in the listbox "musiclist"
@@ -64,6 +70,9 @@ def getselected(event, listbox):
         # Sets the test label to the selected file from the listbox
         pathstring.set(selectedfile)
         print(selectedfile)
+
+        selectedpath = pathlist[flist.index(selectedfile)]
+        print(selectedpath)
 
 
 # Function used to create queue
@@ -109,6 +118,7 @@ pathfinder = Button(root, text="Select Music folder", command=lambda: getfile(mu
 
 #Queue listbox that tests the addtoqu Function
 qulist = Listbox(root, width=55)
+qulist.bind("<Double-Button-1>", lambda event: getselected(event, qulist))
 
 #Test button that adds songs to queue
 qubutton = Button(root, text="Add to Queue", command=lambda: addtoqu(qulist))
@@ -116,13 +126,20 @@ qubutton = Button(root, text="Add to Queue", command=lambda: addtoqu(qulist))
 #Test button that clears the queue
 clearqu = Button(root, text="Clear Queue", command=lambda: quclear(qulist))
 
+#Test play, next and previous buttons
+playbutton = Button(root, text="||>")
+nextbutton = Button(root, text=">|")
+previousbutton = Button(root, text="|<")
 
-pathlabel.grid(row=0, columnspan=2, sticky=EW)
-musiclist.grid(row=1, columnspan=2, sticky=EW)
-pathfinder.grid(row=2, columnspan=2, sticky=EW)
-qubutton.grid(row=3, column=0, sticky=EW)
-clearqu.grid(row=3, column=1, sticky=EW)
-qulist.grid(row=4, columnspan=2, sticky=EW)
+pathlabel.grid(row=0, columnspan=3, sticky=EW)
+musiclist.grid(row=1, columnspan=3, sticky=EW)
+pathfinder.grid(row=2, column=1, sticky=EW)
+qubutton.grid(row=2, column=0, sticky=EW)
+clearqu.grid(row=2, column=2, sticky=EW)
+playbutton.grid(row=4, column=1, sticky=EW)
+nextbutton.grid(row=4, column=0, sticky=EW)
+previousbutton.grid(row=4, column=2, sticky=EW)
+qulist.grid(row=5, columnspan=3, sticky=EW)
 
 
 root.mainloop()
