8a8e022c6eafefcd0f181dd6aacd74e32456e65b
Darth218 <yahia.hany.gaber@gmail.com>
2023-12-09 14:08:31 +0200

    Updated terminal.py

--- terminal.py
+++ terminal.py
@@ -2,6 +2,8 @@
 import curses
 from curses import wrapper
 
+welcome_message = "Welcome to ComMusic!"
+
 def initialise_subwin(scr, w, h, x, y):
     subwindow = scr.subwin(w, h, x, y)
     subwindow.border()
@@ -46,16 +48,15 @@ def main(stdscr):
         1, queue_width * 3
     )
 
-    songs = initialise_subwin(
-        stdscr,
-        songs_height,
-        songs_width,
-        1, playlists_width + 2
-    )
+    songs = curses.newpad(songs_height, songs_width)
 
-    stdscr.addstr("Welcome to ComMusic!")
+    stdscr.addstr(0, curses.COLS // 2 - len(welcome_message) // 2, welcome_message)
 
     stdscr.refresh()
+    songs.border()
+    songs.refresh(0, 0, 1, playlists_width + 2, songs_height, songs_width)
+
     stdscr.getkey()
+
 
 wrapper(main)
