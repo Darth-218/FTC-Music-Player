7d15a5b65e1d80ea24c2169417255c56db79d36f
darth-218 <yahia.hany.gaber@gmail.com>
2023-12-31 20:38:37 +0200

    dded albums to the view, still working on it

--- localfiles.py
+++ localfiles.py
@@ -11,7 +11,7 @@ class Local:
     def __init__(self):
 
         self.songlist = ft.ListView(expand=1, spacing=10, padding=20)
-        self.albumlist = ft.ListView(expand=1, spacing=12, padding=20)
+        self.albumlist = ft.ListView(expand=1, spacing=12, padding=20, horizontal = True)
 
         self.pick_files_dialog = ft.FilePicker(on_result=lambda e: Localclass.pick_files_result(e))
         self.picker = ft.ElevatedButton("Pick Directory", icon=ft.icons.UPLOAD_FILE_ROUNDED, tooltip="Click here to choose a song directory",  on_click=lambda _: self.pick_files_dialog.get_directory_path())
@@ -21,6 +21,9 @@ class Local:
 
         self.song = dm.Song
         self.songmeta = {}
+
+        self.albums = list()
+        self.albumsongs = {}
 
     # Functions
     # Function to get the main music folder's path
@@ -45,20 +48,30 @@ class Local:
                         # Adds all files with ".mp3" extension and their paths to a dictionary"
                         self.filepaths[fname] = os.path.join(path, fname)
 
-                for i in self.filepaths:
+            for i in self.filepaths:
 
-                    self.getdetails(self.filepaths[i])
+                self.getdetails(self.filepaths[i])
 
-                    self.song.name = i
+                self.song.name = i
 
-                    self.localsongwidget(self.songlist,
-                                         self.song.name,
-                                         self.songmeta["artist"],
-                                         self.songmeta["duration"],
-                                         self.songmeta["album"])
+                self.localsongwidget(self.songlist,
+                                        self.song.name,
+                                        self.songmeta["artist"],
+                                        self.songmeta["duration"],
+                                        self.songmeta["album"])
+
+                self.albums.append(self.songmeta["album"])
+
+                self.localalbumwidget(self.albumlist, self.songmeta["album"])
 
         return selected_folder
-    
+
+    def songsinalbum(self, albumnames):
+
+        for i in albumnames:
+
+            self.localalbumwidget(self.albumlist, albumnames, i)
+
     def pick_files_result(self, e: ft.FilePickerResultEvent):
 
         self.folder_name = (e.path if e.path else "Cancelled")
@@ -131,21 +144,34 @@ class Local:
         self.songcontainer.margin = 2
 
         songbox.controls.append(self.songcontainer)
+        # print(f"built {songname}")
 
         songbox.update()
 
+    def localalbumwidget(self, albumbox, albumname: str = "N/A", artistname: str = "N/A"):
+
+        self.albumcontainer = ft.Container(content = ft.Column([ft.Text(albumname), ft.Text(artistname)]))
+
+        self.albumcontainer.border = ft.border.all(1, ft.colors.GREY)
+        self.albumcontainer.border_radius = ft.border_radius.all(7)
+        self.albumcontainer.padding = 5
+        self.albumcontainer.margin = 2
+
+        albumbox.controls.append(self.albumcontainer)
+        # print(f"built {albumname}")
+
+        albumbox.update()
 
 Localclass = Local()
 
 
 def main(page: ft.Page):
 
-
     page.overlay.append(Localclass.pick_files_dialog)
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
 
-    page.add(ft.Column([Localclass.songlist, Localclass.picker], expand = 1))
+    page.add(ft.Column([Localclass.albumlist, Localclass.songlist, Localclass.picker], expand = 1))
 
     page.update()
 
